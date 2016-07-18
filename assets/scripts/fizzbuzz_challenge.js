ec9a6b828debb8e5eac1fd6b8e2a769fe8b703c2
jvjruiz <jovincent.ruiz@gmail.com>
2016-07-18 09:46:42 -0700

    finished

---
+++ assets/scripts/fizzbuzz_challenge.js
@@ -0,0 +1,36 @@
+var countto = parseInt(prompt("What number shall we count to?"))
+
+var fizzarray = function(number){
+	array =  []
+	for(var i = 0; i <= number; i++){
+		array.push(i);
+	}
+	return array
+}
+
+var fizzbuzz = function(array){
+	for (var i in array){
+		if(i % 3 === 0 && i % 5 ===0){
+			print("Fizzbuzz");
+		}
+		else if(i % 3 === 0){
+			print("Fizz");
+		}
+		else if(i % 5 === 0){
+			print("Buzz")
+		}
+		else{
+			print(i);
+		}
+	}
+}
+
+
+var r = document.getElementById('result');
+
+function print(s){
+	r.innerHTML += s + '<br>';
+}
+
+
+fizzbuzz(fizzarray(countto))
---
+++ fizzbuzz_challenge.jss
@@ -0,0 +1,15 @@
+var countto = parseInt(prompt("What number shall we count to?"))
+
+var fizzarray = function(int){
+	array =  []
+}
+
+
+
+
+
+var r = document.getElementById('result');
+
+function print(s){
+	r.innerHTML += s + '<br>';
+}
